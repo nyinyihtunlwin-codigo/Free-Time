616e553f77582433cdfc1ff4d4e054a43a59a460
Nyi Nyi Htun Lwin <nnhl.mgk@gmail.com>
2018-02-06 18:24:20 +0630

    Movie Details Screen UI Updated.

--- app/src/main/java/projects/nyinyihtunlwin/zcar/activities/MovieDetailsActivity.java
+++ app/src/main/java/projects/nyinyihtunlwin/zcar/activities/MovieDetailsActivity.java
@@ -1,5 +1,7 @@
 package projects.nyinyihtunlwin.zcar.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -8,13 +10,17 @@ import projects.nyinyihtunlwin.zcar.R;
 
 public class MovieDetailsActivity extends AppCompatActivity {
 
+    public static Intent newIntent(Context context) {
+        Intent intent = new Intent(context, MovieDetailsActivity.class);
+        return intent;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
     }
 
 }
