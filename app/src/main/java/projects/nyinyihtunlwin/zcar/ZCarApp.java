5e1b297deec893363bc66321701dca833c7be264
Nyi Nyi Htun Lwin <nnhl.mgk@gmail.com>
2018-04-04 15:21:04 +0630

    Fabric Added.

--- app/src/main/java/projects/nyinyihtunlwin/zcar/ZCarApp.java
+++ app/src/main/java/projects/nyinyihtunlwin/zcar/ZCarApp.java
@@ -2,6 +2,8 @@ package projects.nyinyihtunlwin.zcar;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import projects.nyinyihtunlwin.zcar.data.models.MovieModel;
 import projects.nyinyihtunlwin.zcar.utils.AppConstants;
 import projects.nyinyihtunlwin.zcar.utils.AppUtils;
@@ -17,6 +19,7 @@ public class ZCarApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         ConfigUtils.initConfigUtils(getApplicationContext());
         AppUtils.initAppUtils(getApplicationContext());
         MovieModel.getInstance().startLoadingMovieGenres(getApplicationContext());
